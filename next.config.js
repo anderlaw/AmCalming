f10d6e2f7077257215fec245eff88cb9c47f6337
andylaw <974088545@qq.com>
2022-03-17 22:48:33 +0800

    chore: config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: "./",
+  // 编译文件的输出目录
+  // distDir: "dist/",
 }
 
 module.exports = nextConfig
