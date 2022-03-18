0e27879c8591ac1e0c2042cbbc3554537dbc7b50
andylaw <974088545@qq.com>
2022-03-18 17:40:22 +0800

    ci: update rename script

--- rename_next.js
+++ rename_next.js
@@ -1,21 +1,52 @@
 const fs = require('fs')
 const files = fs.readdirSync('./out')
+const renameFileAndRecord = (absoluteDirName,preResult) => {
+    const files = fs.readdirSync(absoluteDirName)
+    files.forEach(name => {
+        const stat = fs.lstatSync(absoluteDirName+'/'+name)
+        let newName = name
+        if(name.indexOf('_') === 0){
+            newName = name.slice(1)
+            //record
+            preResult.push(name)
+            //rename
+            fs.renameSync(`${absoluteDirName}/${name}`,`${absoluteDirName}/${newName}`)
+        }
+
+        if(!stat.isFile()){
+            renameFileAndRecord(`${absoluteDirName}/${newName}`,preResult)
+        }
+    })
+}
+const renameResult = []
 files.forEach(name => {
     /**
      * rename _next to next
      * replace all the `_next` to `next` in html file
      */
     if(name === '_next'){
-        fs.renameSync(`${__dirname}/out/${name}`,`${__dirname}/out/${'next'}`)
-        console.log('重命名 _next 为 next')
+        const newName = name.slice(1)
+        fs.renameSync(`${__dirname}/out/${name}`,`${__dirname}/out/${newName}`)
+
+        renameResult.push(name)
+        renameFileAndRecord(`${__dirname}/out/${newName}`,renameResult)
     }
+    console.log('需要替换的文件或目录-->',renameResult)
+})
+
+files.forEach(name => {
     if(name.indexOf('.html') > -1){
-        const htmlContent = fs.readFileSync(`${__dirname}/out/${name}`,{
+        let htmlContent = fs.readFileSync(`${__dirname}/out/${name}`,{
             encoding:'utf8'
         })
-        let newHtmlContent = htmlContent.replaceAll('./_next','./next')
-        fs.writeFileSync(`${__dirname}/out/${name}`,newHtmlContent)
+        renameResult.forEach(catchedName => {
+            htmlContent = htmlContent.replaceAll(`/${catchedName}`,`/${catchedName.slice(1)}`)
+        })
+        fs.writeFileSync(`${__dirname}/out/${name}`,htmlContent)
         console.log(`${name} 内容替换成功！`)
     }
-    console.log('运行完毕')
 })
+
+console.log('运行完毕')
+
+
