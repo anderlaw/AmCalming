e07343e01bbc13e75b907fb0e164ff0773adc49d
andylaw <974088545@qq.com>
2022-03-20 13:26:05 +0800

    chore: test

--- components/main/index.tsx
+++ components/main/index.tsx
@@ -10,6 +10,8 @@ interface IProps {
 export const MainContainer:FC<IProps> = (props) => {
     return <div className={mainStyles.mainContainer}>
         <audio src={'https://full-audio-resource-1256270265.cos.ap-shanghai.myqcloud.com/Birds.mp3'} controls></audio>
+        <br/>
+        <audio src={'https://audio-resource-1256270265.cos.ap-shanghai.myqcloud.com/audio/%E5%B0%8F%E9%B8%A1.mp3'} controls></audio>
         {
             props.musicNameList.map(name => {
                 return <div className={`${mainStyles.musicItemContainer} ${props.curPlaying.find(item => item.audioPlaying && item.audioName === name) ? mainStyles.active:''}`} key={name} onClick={props.onMusicClick.bind(null,name)}>{name}</div>
