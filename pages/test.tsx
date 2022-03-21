22550fdfd4afd1d5df61f2e5b44c19c5ed7b3c3f
andylaw <974088545@qq.com>
2022-03-21 19:57:20 +0800

    chore: test

--- pages/test.tsx
+++ pages/test.tsx
@@ -7,29 +7,28 @@ export default () => {
         newAudioEle.controls = false
         newAudioEle.loop = true
         newAudioEle.src = '/Afternoon.mp3'
+        newAudioEle.volume = 0.4
+        // const newAudioCtx = new AudioContext();
+        // const gainNode = newAudioCtx.createGain();
 
-        const newAudioCtx = new AudioContext();
-        const gainNode = newAudioCtx.createGain();
-
-        const finish = newAudioCtx.destination;
-        newAudioCtx.createMediaElementSource(newAudioEle).connect(gainNode)
-        gainNode.connect(newAudioCtx.destination)
+        // const finish = newAudioCtx.destination;
+        // newAudioCtx.createMediaElementSource(newAudioEle).connect(gainNode)
+        // gainNode.connect(newAudioCtx.destination)
         //音量
-        gainNode.gain.value = 2
+        // gainNode.gain.value = 2
         //insert into body
-        document.body.appendChild(newAudioEle)
+        // document.body.appendChild(newAudioEle)
         newAudioEle.play()
-        setPlayContext({
-            context:newAudioCtx,
-            gainNode:gainNode,
-            audioEle:newAudioEle
-        })
+        // setPlayContext({
+        //     context:newAudioCtx,
+        //     gainNode:gainNode,
+        //     audioEle:newAudioEle
+        // })
         setInterval(()=>{
             console.log('当前播放位置：',newAudioEle.currentTime)
             console.log('是否结束播放：',newAudioEle.ended)
             console.log('是否暂停播放：',newAudioEle.paused)
-            console.log('所属context：',newAudioCtx)
-            console.log('音轨声音：',gainNode.gain.value)
+            console.log('音轨声音：',newAudioEle.volume)
         },3000)
     }}>
         play
